fc15f3cbd0e71b554e6f8b4d8e8b82accb810ecc
CongWeilin <congweilin95@gmail.com>
2021-08-07 00:26:00 -0400

    reproduce GCN F1-score results

---
+++ example.sh
@@ -0,0 +1,8 @@
+python run_exps_diff_layers.py --dataset cora --method GCN --epoch_num 10000 --n_layers 2
+python run_exps_diff_layers.py --dataset cora --method GCN --epoch_num 10000 --n_layers 3
+python run_exps_diff_layers.py --dataset cora --method GCN --epoch_num 10000 --n_layers 4
+python run_exps_diff_layers.py --dataset cora --method GCN --epoch_num 10000 --n_layers 5
+python run_exps_diff_layers.py --dataset cora --method GCN --epoch_num 10000 --n_layers 6
+python run_exps_diff_layers.py --dataset cora --method GCN --epoch_num 10000 --n_layers 7
+python run_exps_diff_layers.py --dataset cora --method GCN --epoch_num 10000 --n_layers 8
+python run_exps_diff_layers.py --dataset cora --method GCN --epoch_num 10000 --n_layers 9
